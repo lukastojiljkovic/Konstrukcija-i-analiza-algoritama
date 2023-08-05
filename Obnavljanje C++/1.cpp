31d9f4c2a5c0e7aec9f7b20c4abb43ca71b7fb87
Luka Stojiljkovic <luka.d.stojiljkovic@gmail.com>
2023-08-05 18:01:20 +0200

    Stablo

--- Obnavljanje C++/1.cpp
+++ Obnavljanje C++/1.cpp
@@ -15,11 +15,69 @@
 #include <functional>
 using namespace std;
 
+struct Cvor {
+   Cvor(int v) {
+      vr = v;
+   }
+
+   int vr;
+   Cvor* l = nullptr;
+   Cvor* d = nullptr;
+};
+
+Cvor* dodaj(Cvor* t, int v) {
+   if (t == nullptr) {
+      return new Cvor(v);
+   }
+   if (v < t->vr) {
+      t->l = dodaj(t->l, v);
+   } else {
+      t->d = dodaj(t->d, v);
+   }
+   return t;
+}
+
+void oslobodi(Cvor* t) {
+   if (t == nullptr) {
+      return;
+   }
+   oslobodi(t->l);
+   oslobodi(t->d);
+   delete t;
+   t = nullptr;
+   return;
+}
+
+void ispisi(Cvor* t) {
+   if (t == nullptr) {
+      return;
+   }
+   ispisi(t->l);
+   cout << t->vr << " ";
+   ispisi(t->d);
+   return;
+}
+
+
 int main(int argc, char const *argv[]) {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     
-   
+   int n;
+   cin >> n;
+
+   Cvor* t = nullptr;
+   while(n--) {
+      int v;
+      cin >> v;
+      t = dodaj(t, v);
+   }
+
+   //ispis
+   ispisi(t);
+   cout << endl;
+   //oslobodi
+   oslobodi(t);
     
     return 0;
 }
