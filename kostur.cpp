bbc01b831b11fd7f733efe2a62907b6bf55e1bcb
Luka Stojiljkovic <luka.d.stojiljkovic@gmail.com>
2023-08-05 17:25:04 +0200

    Initial commit

---
+++ kostur.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <iomanip>
+#include <unordered_map>
+#include <vector>
+#include <stack>
+#include <utility>
+#include <numeric>
+#include <cstring>
+#include <functional>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    
+    
+    return 0;
+}
